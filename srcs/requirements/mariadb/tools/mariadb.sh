2f28734cbfebad0979921b7fade0ddb954a44f99
RenHartt <141726227+RenHartt@users.noreply.github.com>
2024-09-28 18:20:31 +0200

    Update mariadb.sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -6,7 +6,6 @@ mysql_install_db
 
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
-
 	echo "Database already exists"
 else
 
@@ -21,8 +20,11 @@ Y
 Y
 EOF
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -u root
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
 
 fi
 
